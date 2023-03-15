22017347f768ce2082201cfc2d9a8d638158251d
Stefko Tsonyovski <stefko.tsonyovski@scalefocus.com>
2023-03-15 15:39:19 +0200

    Initial Commit

---
+++ src/components/Banner.js
@@ -0,0 +1,108 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = ["Web Developers", "And", "Mobile Developers"];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex((prevIndex) => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <span className="tagline">Welcome to our Portfolio</span>
+                  <h1>
+                    {`Hi! We are Stefko and Miroslav`}{" "}
+                    <span
+                      className="txt-rotate"
+                      dataPeriod="1000"
+                      data-rotate='[ "Web Developers", "And", "Mobile Developers" ]'
+                    >
+                      <span className="wrap">{text}</span>
+                    </span>
+                  </h1>
+                  <p>
+                    Are you looking for expert web and mobile developers to
+                    develop your web or mobile app? If your answer is yes, then
+                    we welcome you and you are at the right place. We are
+                    professional web and mobile developers for 3+ years. We
+                    develop professional web and mobile apps using our skills
+                    and techniques. You can see some of our projects below and
+                    visit our page in Fiverr for more details about the
+                    technologies we use!
+                  </p>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                >
+                  <img src={headerImg} alt="Header Img" />
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
---
+++ src/components/Projects.js
@@ -0,0 +1,173 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
+export const Projects = () => {
+  const omcs = [
+    {
+      title: "OMCS",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "OMCS",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "OMCS",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "OMCS",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "OMCS",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "OMCS",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  const tdt = [
+    {
+      title: "TennisDreamTeam",
+      description: "Match Details",
+      imgUrl:
+        "https://res.cloudinary.com/dcvkhhwth/image/upload/v1678883021/match-details_psacj5.jpg",
+    },
+    {
+      title: "TennisDreamTeam",
+      description: "Rankings",
+      imgUrl:
+        "https://res.cloudinary.com/dcvkhhwth/image/upload/v1678883021/ranking_ythjcc.jpg",
+    },
+    {
+      title: "TennisDreamTeam",
+      description: "Games",
+      imgUrl:
+        "https://res.cloudinary.com/dcvkhhwth/image/upload/v1678883021/games_1_ixruxo.jpg",
+    },
+    {
+      title: "TennisDreamTeam",
+      description: "Fantasy Team Part 1",
+      imgUrl:
+        "https://res.cloudinary.com/dcvkhhwth/image/upload/v1678883021/fantasy_1_hsde2l.jpg",
+    },
+    {
+      title: "TennisDreamTeam",
+      description: "Fantasy Team Part 2",
+      imgUrl:
+        "https://res.cloudinary.com/dcvkhhwth/image/upload/v1678883021/fantasy_3_xltpnh.jpg",
+    },
+    {
+      title: "TennisDreamTeam",
+      description: "Brackets",
+      imgUrl:
+        "https://res.cloudinary.com/dcvkhhwth/image/upload/v1678883021/brackets_ciqfhe.jpg",
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Projects</h2>
+                  <p>Enough talk! Let's see some of our projects:</p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">OMCS</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">TDT</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">HRM</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <p>
+                          Online Course Management System or OMCS is a system
+                          for managing online courses developed with .NET stack
+                          (C#, ASP .NET Core 5.0, Entity Framework Core 5.0,
+                          xUnit, Blazor, SignalR, Automapper). It has different
+                          modules such as Dashboard, Events Panel, Assignments,
+                          Exams, Forum, Q&A, Video Conference and Courses. This
+                          project was awarded with 3rd place at John Atanasoff
+                          IT tournament and earned excellent mark at NOIT
+                          (National Olympiad of Information Technologies)
+                        </p>
+                        <Row>
+                          {omcs.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <p>
+                          Welcome to TennisDreamTeam! If you want to test your
+                          tennis knowledge, compete with others, participate in
+                          leagues or just to have fun, you are at the right
+                          place! We offer three different types of games which
+                          at the end are used to calculate your total points.
+                          The project is developed using native MERN stack
+                          (MongoDB, ExpressJS, React Native and Node.js)
+                        </p>
+                        <Row>
+                          {tdt.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Cumque quam, quod neque provident velit, rem
+                          explicabo excepturi id illo molestiae blanditiis,
+                          eligendi dicta officiis asperiores delectus quasi
+                          inventore debitis quo.
+                        </p>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  );
+};
